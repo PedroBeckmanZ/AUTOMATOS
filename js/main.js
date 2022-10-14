5d72e1b11047d14b4df512e2c027a780ce9a45cb
PedroBeckmanZ <pedroluizbeckman@gmail.com>
2022-10-14 09:14:50 -0300

    Primeiro commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$('.slider-principal').slick({
+
+    dots: true,
+    infinite: true,
+    speed: 150,
+    slidesToShow: 1,
+    adaptativeHeight: true,
+    autoplay: true,
+    autoplaySpeed: 3000
+
+})
+
